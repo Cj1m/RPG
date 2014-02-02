d98cc6e5ccf6c81bc1058e15bdd6d92c8c350afc
cj1m <cj1mckay@gmail.com>
2014-02-02 14:37:52 +0000

    Needs Fixing

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -7,24 +7,40 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
-public class Main extends BasicGame
-{
+public class Main extends BasicGame{
+	SpriteSheet playerSprites;
+	Image player;
+	Input input;
+	int x;
+	int y;
+	
 	public Main(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}
+	public void init(GameContainer gc) throws SlickException {
+		playerSprites = new SpriteSheet("gfx/Sprites.png", 16, 24);
+		
+		player = playerSprites.getSprite(0, 0);
+	}
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException {
+		if(input.isKeyDown(Input.KEY_W)){
+			y++;
+		}
+	}
 
 	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException
-	{
+	public void render(GameContainer gc, Graphics g) throws SlickException{
+		player.draw(x,y);
 	}
 
 	
